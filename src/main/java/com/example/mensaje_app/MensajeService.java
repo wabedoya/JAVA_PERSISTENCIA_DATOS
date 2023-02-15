74ac7c15b32bd8313d9feb6f6d9a360be866a460
Wilson Alexis Bedoya <wilson.bedoya24@gmail.com>
2023-02-15 11:17:56 -0500

    creacion clase y metodos para ingresar datos a la base de datos

--- src/main/java/com/example/mensaje_app/MensajeService.java
+++ src/main/java/com/example/mensaje_app/MensajeService.java
@@ -1,8 +1,19 @@
 package com.example.mensaje_app;
 
+import java.util.Scanner;
+
 public class MensajeService {
 
     public static void crearMensaje(){
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Por favor escribe tu mensaje");
+        String mensaje = scanner.nextLine();
+        System.out.println("Por favor tu nombre");
+        String nombre = scanner.nextLine();
+        Mensajes registro = new Mensajes();
+        registro.setMensaje(mensaje);
+        registro.setAutor_mensaje(nombre);
+        MensajesDAO.crearMensajeDB(registro);
 
     }
     public static void listarMensaje(){
--- src/main/java/com/example/mensaje_app/MensajesDAO.java
+++ src/main/java/com/example/mensaje_app/MensajesDAO.java
@@ -1,7 +1,29 @@
 package com.example.mensaje_app;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MensajesDAO {
     public static void crearMensajeDB(Mensajes mensaje){
+        Conexion db_connect = new Conexion();
+        try(Connection conexion= db_connect.get_connection()){
+            PreparedStatement ps = null;
+            try {
+                String query = "INSERT INTO mensajes (mensaje, autor_mensaje) VALUES (?, ?)";
+                ps = conexion.prepareStatement(query);
+                ps.setString(1, mensaje.getMensaje());
+                ps.setString(2,mensaje.getAutor_mensaje());
+                ps.executeUpdate();
+                System.out.println("Mensaje creado y guaradado en base de datos");
+            }catch (SQLException e){
+                System.out.println(e);
+            }
+
+        }catch (SQLException e){
+            System.out.println(e);
+
+        }
 
     }
     public static void leerMensajeDB(){
