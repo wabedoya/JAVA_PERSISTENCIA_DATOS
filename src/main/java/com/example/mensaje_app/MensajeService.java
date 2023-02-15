7f85046da6a79ab6a2ada2908573cd96ff3240d7
Wilson Alexis Bedoya <wilson.bedoya24@gmail.com>
2023-02-15 13:30:48 -0500

    creacion de los metodos crear, borrar, actualizar y listar

--- src/main/java/com/example/mensaje_app/MensajeService.java
+++ src/main/java/com/example/mensaje_app/MensajeService.java
@@ -17,12 +17,25 @@ public class MensajeService {
 
     }
     public static void listarMensaje(){
+        MensajesDAO.leerMensajeDB();
 
     }
     public static void borrarMensaje(){
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Por favor ID mensaje a borrar");
+        int id_mensaje = scanner.nextInt();
+        MensajesDAO.borrarMensajeDB(id_mensaje);
 
     }
     public static void editarMensaje(){
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Escribir nuevo mensaje");
+        String mensaje = scanner.nextLine();
+        System.out.println("ID mensaje actualizar");
+        int id_mensaje = scanner.nextInt();
+        Mensajes actualizacion = new Mensajes();
+        actualizacion.setMensaje(mensaje);
+        MensajesDAO.actualizaerMensajeDB(actualizacion);
 
     }
 }
--- src/main/java/com/example/mensaje_app/MensajesDAO.java
+++ src/main/java/com/example/mensaje_app/MensajesDAO.java
@@ -2,6 +2,7 @@ package com.example.mensaje_app;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MensajesDAO {
@@ -19,20 +20,70 @@ public class MensajesDAO {
             }catch (SQLException e){
                 System.out.println(e);
             }
+        }catch (SQLException e){
+            System.out.println(e);
+        }
+    }
+    public static void leerMensajeDB(){
+        Conexion db_connect = new Conexion();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try(Connection conexion= db_connect.get_connection()){
+            String query = "SELECT * FROM mensajes";
+            ps = conexion.prepareStatement(query);
+            rs= ps.executeQuery();
+            while (rs.next()){
+                System.out.println("ID: "+rs.getInt("id_mensaje"));
+                System.out.println("Mensaje : "+rs.getString("mensaje"));
+                System.out.println("Autor: "+ rs.getString("autor_mensaje"));
+                System.out.println("Fecha: "+rs.getString("fecha"));
+                System.out.println(" ");
+            }
+        }catch (SQLException e){
+            System.out.println(e);
+        }
+    }
+    public static void borrarMensajeDB(int id_mensaje){
+        Conexion db_connect = new Conexion();
+        try(Connection conexion= db_connect.get_connection()){
+            PreparedStatement ps = null;
+            try {
+                String query = "DELETE FROM mensajes WHERE id_mensaje = ?";
+                ps = conexion.prepareStatement(query);
+                ps.setInt(1, id_mensaje);
+                ps.executeUpdate();
+                System.out.println("Mensaje Borrado");
 
+
+            }catch(SQLException e){
+                System.out.println(e);
+                System.out.println("No se pudo borrar el mensaje ");
+            }
+        }catch (SQLException e){
+            System.out.println(e);
+        }
+    }
+    public static void actualizaerMensajeDB(Mensajes mensaje){
+        Conexion db_connect = new Conexion();
+        try(Connection conexion= db_connect.get_connection()){
+            PreparedStatement ps = null;
+            try {
+                String query = "UPDATE mensajes SET mensaje=? WHERE id_mensaje = ?";
+                ps = conexion.prepareStatement(query);
+                ps.setString(1, mensaje.getMensaje());
+                ps.setInt(2, mensaje.getId_mensajes());
+                ps.executeUpdate();
+                System.out.println("Mensaje Actualizado");
+            }catch(SQLException e){
+                System.out.println(e);
+                System.out.println("No se pudo actualizar el mensaje ");
+            }
         }catch (SQLException e){
             System.out.println(e);
 
         }
 
-    }
-    public static void leerMensajeDB(){
 
-    }
-    public static void borrarMensajeDB(int id_mensaje){
-
-    }
-    public static void actualizaerMensajeDB(Mensajes mensaje){
 
     }
 }
